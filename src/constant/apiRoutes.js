6b02c1096ddd2b8171c266bb9b296db01b5e7797
parthivvora <v.parthiv004@gmail.com>
2024-03-17 23:11:04 +0530

    Fix: Create apiRoutes for project

---
+++ src/constant/apiRoutes.js
@@ -0,0 +1,10 @@
+const apiRoutes = {
+    HOME: "/",
+    REGISTER: "/register",
+    LOGIN: "/login",
+    ABOUT: "/about",
+    CONTACT: "/contact",
+    MOVIE: "/movie",
+}
+
+export default apiRoutes
